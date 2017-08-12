ca780dd1fb656aed471092164c7714176a382acc
Ignacio Baixas <iobaixas@gmail.com>
2017-08-12 18:19:54 -0400

    feat(spec): adds adds contract construction integration spec

---
+++ spec/integration/contract_construction_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Test contract interaction', integration: true do
+  let(:abi_location) do
+    File.expand_path('./spec/test_contract/build/contracts/ConstructorTest.json')
+  end
+
+  let(:contract_class) { Etherlite::Abi.load_contract_at abi_location }
+
+  it "properly deploys a contract that receives constructor arguments" do
+    uint = rand(100)
+    string = "string_#{rand(100)}"
+
+    contract = contract_class.deploy uint, string, client: client, gas: 1000000
+
+    expect(contract.uint_param).to eq uint
+    expect(contract.string_param).to eq string
+  end
+end
