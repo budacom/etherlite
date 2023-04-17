77d453377e839a81da4ec42691c910ce14931111
Ignacio Baixas <ignacio@buda.com>
2023-04-17 16:22:24 -0400

    fix: fixes option processing on Etherlite.connect method

--- spec/etherlite_spec.rb
+++ spec/etherlite_spec.rb
@@ -5,8 +5,20 @@ describe Etherlite do
     expect(Etherlite::VERSION).not_to be nil
   end
 
-  describe ".connection" do
-    it "returns the default connection" do
+  describe '.connect' do
+    it 'passes the base uri to the new connection' do
+      client = Etherlite.connect('http://foo.bar')
+      expect(client.connection.uri.to_s).to eq 'http://foo.bar'
+    end
+
+    it 'passes the chain_id option to the new connection' do
+      client = Etherlite.connect('http://foo.bar', chain_id: 123)
+      expect(client.connection.chain_id).to eq 123
+    end
+  end
+
+  describe '.connection' do
+    it 'returns the default connection' do
       expect(Etherlite.connection).to be_a Etherlite::Connection
     end
   end
